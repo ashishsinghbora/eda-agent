ffedd1e1cdcf02ee57d46ae1ed551607df723e15
ashishsinghbora <mr.ashishsinghbora@gmail.com>
2026-08-25 05:21:50 +0530

    chore: update gitignore to ignore simulation build artifacts and vvp binaries

--- examples/sim/sim_build_alu_8bit/cocotb_iverilog_dump.v
+++
@@ -1,6 +0,0 @@
-module cocotb_iverilog_dump();
-initial begin
-    $dumpfile("sim_build_alu_8bit/alu_8bit.fst");
-    $dumpvars(0, alu_8bit);
-end
-endmodule
--- examples/sim/sim_build_fifo_async/cocotb_iverilog_dump.v
+++
@@ -1,6 +0,0 @@
-module cocotb_iverilog_dump();
-initial begin
-    $dumpfile("sim_build_fifo_async/fifo_async.fst");
-    $dumpvars(0, fifo_async);
-end
-endmodule
